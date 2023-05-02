f41f1751effafca07be07b5d7be7e18fb40b8409
louisaiva <deltasfer@gmail.com>
2023-05-02 09:41:09 +0200

    reception uart !

--- py/transmission.py
+++ py/transmission.py
@@ -6,49 +6,29 @@ port_uart = "/dev/serial0"
 path = "/var/www/html/instruction.txt"
 baudrate = 19200
 
-# windows tests
-# port_uart = "COM2"
-# path_wr = "vitesse.txt"
-# path = "instruction.txt"
-# baudrate = 9600
-
 
 bytesize = 8
 
 
-def receive():
-
-    # Ouverture de la liaison UART
-    # uart = serial.Serial(port_uart, baudrate, bytesize)
-    # uart.timeout = 0.1
-
-    # Récupération de caracètres
-    msg = uart.read(100).decode()
-    if (msg != ''):
-            print("[... -> RASP] << " + str(msg))
-
-    # uart.close()
-
-    return msg
-
-def send(c):
-
-    # Ouverture de la liaison UART
-    # uart = serial.Serial(port_uart, baudrate, bytesize) #stopbits, serial.PARITY_NONE, stopbits, timeout, xonxoff, rtscts
-
-    # Ecriture d'un caractère de test 'a'
-    print("[RASP -> ...] >> "+str(c))
-    uart.write(c.encode('utf-8'))
-
-    # uart.close()
-
 uart = serial.Serial(port_uart,baudrate,bytesize)
 uart.timeout = 0.1
 
-while 1:
-    # sleep
-    # time.sleep(0.1)
 
+
+def receive():
+    # Récupération de caracètres
+    msg = uart.read(100).decode()
+    if msg != '':
+            print("(reception) < " + str(msg))
+    return msg
+
+def send(c):
+    # Ecriture de caractères
+    print("            > "+str(c))
+    uart.write(c.encode('utf-8'))
+
+
+while 1:
     # lecture des instructions
     f=open(path,'r')
     instruction=f.read()
@@ -56,7 +36,7 @@ while 1:
 
     # on envoie l'instruction
     send(instruction)
-
+    
     # on recoit les informations
     receive()
 
